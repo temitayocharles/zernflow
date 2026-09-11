0937874253a84f712163f83c00ddbda712c19396
temitayocharles <temitayo_charles@yahoo.com>
2026-09-11 23:09:53 +0000

    sync: import vitest.config.ts from Arena

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     },
   },
   test: {
-    include: ["**/*.test.ts"],
+    include: ["**/*.test.ts", "**/*.test.tsx"],
     exclude: ["node_modules/**"],
     environment: "node",
   },
